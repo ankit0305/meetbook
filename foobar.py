c6183017348331fe4763f7655c167f9bf0d31b59
ankit0305 <jaiswal.ankit2000@gmail.com>
2023-01-02 14:36:09 +0530

    removing keys

---
+++ foobar.py
@@ -0,0 +1,1 @@
+djangoKey = 'django-insecure-!fjn9d)ku34-@gvimi8myqy9t0xvag5$g%7_7894ta21^rib=n'
--- meetbook/settings.py
+++ meetbook/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from foobar import djangoKey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!fjn9d)ku34-@gvimi8myqy9t0xvag5$g%7_7894ta21^rib=n'
+SECRET_KEY = djangoKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
